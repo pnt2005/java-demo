b67454cc442a9ad2a6a9cd95afa147429c6dc8e4
pnt2005 <85752837+pnt2005@users.noreply.github.com>
2025-03-02 14:53:12 +0700

    add comment

--- utils/MapUtil.java
+++ utils/MapUtil.java
@@ -4,10 +4,10 @@ import java.util.Map;
 
 public class MapUtil {
 	public static <T> T getObject(Map<String, Object> params, String key, Class<T> tClass) {
-		Object obj = params.getOrDefault(key, null);
+		Object obj = params.getOrDefault(key, null);	//lấy giá trị từ params với key, nếu không có thì mặc định là null
 		if (obj != null) {
 			if (tClass.getTypeName().equals("java.lang.Long")) {
-				obj = obj != "" ? Long.valueOf(obj.toString()) : null;
+				obj = obj != "" ? Long.valueOf(obj.toString()) : null;	//Object -> String -> Long
 			} else if (tClass.getTypeName().equals("java.lang.Integer")) {
 				obj = obj != "" ? Integer.valueOf(obj.toString()) : null;
 			} else if (tClass.getTypeName().equals("java.lang.String")) {
